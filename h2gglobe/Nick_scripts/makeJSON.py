e16528579d04c6ce0a5b330a0900f0366b08004c
nckw <>
2011-04-23 17:23:47 +0000

    makeJSON a little better now,

--- h2gglobe/Nick_scripts/makeJSON.py
+++ h2gglobe/Nick_scripts/makeJSON.py
@@ -1,14 +1,25 @@
 from JSON import JSON
+
+outfile=open("goodRuns_cpp.txt",'w')
+
+print "Parsing JSON.py"
+counts = 0
 for p in JSON:
- print "	|| ( run==%d && ("%int(p)
- if len(JSON[p]) ==1:
-  l = JSON[p][0]
-  print " 	(lumis > %d && lumis < %d)"%(l[0],l[1])
+ if counts == 0: 
+  outfile.write("if(   ( run==%d && (\n"%int(p))
  else:
-  l = JSON[p][0]
-  print " 	(lumis > %d && lumis < %d)"%(l[0],l[1])
-  for l in (JSON[p])[1:]:
-         
-	 print "  	|| (lumis > %d && lumis < %d)" %(l[0],l[1])
- print "   	)"
- print "       )"
+  outfile.write("   || ( run==%d && (\n"%int(p))
+ if len(JSON[p]) ==1:
+   l = JSON[p][0]
+   outfile.write( " 	(lumis > %d && lumis < %d)\n"%(l[0],l[1]))
+ else:
+   l = JSON[p][0]
+   outfile.write( " 	(lumis > %d && lumis < %d)\n"%(l[0],l[1]))
+   for l in (JSON[p])[1:]:
+	 outfile.write( "  	|| (lumis > %d && lumis < %d)\n" %(l[0],l[1]))
+ outfile.write( "   	)\n")
+ outfile.write( "  	)\n")
+ counts+=1
+
+outfile.write( "  )\n")
+print "%d Runs saved to goodRuns.txt"%counts
