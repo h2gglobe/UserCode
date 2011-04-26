c5762a17f5e757ef20b99f168c11b60ee91695f5
nckw <>
2011-04-26 23:02:42 +0000

    fixes

--- h2gglobe/PhotonAnalysis_scripts/python/makeFileList.py
+++ h2gglobe/PhotonAnalysis_scripts/python/makeFileList.py
@@ -2,6 +2,7 @@ import commands,sys
 
 def makeDcFiles(dir):
 
+   dcache_prepend = 'dcap://gfe02.grid.hep.ph.ic.ac.uk:22128//pnfs/hep.ph.ic.ac.uk/'
    dir = str(dir)
    return_files = []
 
@@ -13,7 +14,7 @@ def makeDcFiles(dir):
         if len(f) < 1: continue
         f = (f.split()[-1]).split('/')[-1]
 	if '.root' in f:
-	  return_files.append('dcap://gfe02.grid.hep.ph.ic.ac.uk:22128/'+dir+'/'+f)
+	  return_files.append(dcache_prepend+dir+'/'+f)
 
    else:
     sys.exit("No Such Directory: %s"%(dir))
